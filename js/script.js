d50e46ca844b889a9ffaa0b31548fbd64035ce62
Ugo <okekeugochukwu.dev@gmail.com>
2023-04-07 20:07:05 +0100

    Init. { Mobile view done }

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function CLOSE_SIDEBAR() {
+   document.querySelector('.sidebar').classList.add('closed')
+}
+
+function OPEN_SIDEBAR() {
+   document.querySelector('.sidebar').classList.remove('closed')
+}
