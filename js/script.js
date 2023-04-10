8b94f522f080bffa8f6b4914327140c9becb7973
Ugo <okekeugochukwu.dev@gmail.com>
2023-04-10 18:39:24 +0100

    Tablet view done. { Desktop in progress }

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
 function CLOSE_SIDEBAR() {
-   document.querySelector('.sidebar').classList.add('closed')
+   document.querySelector('.sidebar').classList.add('closed');
 }
 
 function OPEN_SIDEBAR() {
-   document.querySelector('.sidebar').classList.remove('closed')
+   document.querySelector('.sidebar').classList.remove('closed');
+}
+
+var animationStatus = false;
+var animationControlBtn = document.querySelector('.animation-control');
+var staticMagicEye = document.querySelector('.magic-eye-static');
+var animatedMagicEye = document.querySelector('.magic-eye-ani');
+var animatedMagicBrush = document.querySelector('.magic-brush-ani');4
+
+function MINI_HOVER() {
+   animationControlBtn.style.bottom = '4vh'
+}
+
+function MINI_HOVER_ALT() {
+   animationControlBtn.style.bottom = '5vh'
+}
+
+function TOGGLE_ANIMATION() {
+   if (animationStatus === true) {
+      animationStatus = false
+      animatedMagicBrush.classList.add('hidden');
+
+      staticMagicEye.classList.remove('hidden');
+      staticMagicEye.classList.add('shown');
+
+      animatedMagicEye.classList.remove('shown');
+      animatedMagicEye.classList.add('hidden');
+   }
+   else {
+      animationStatus = true
+      animatedMagicBrush.classList.remove('hidden');
+
+
+      staticMagicEye.classList.remove('shown');
+      staticMagicEye.classList.add('hidden');
+      
+      animatedMagicEye.classList.remove('hidden');
+      animatedMagicEye.classList.add('shown');
+   }
 }
